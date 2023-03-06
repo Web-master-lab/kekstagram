f89d0111c731ab819ea63fd42f091efdf66281ba
Gleb <stepanov-gleb2003@mail.ru>
2023-03-06 16:35:15 +0300

    Создал модуль утилиты

---
+++ js/util.js
@@ -0,0 +1,19 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
+
+const getRandomArrayOfNumbers = (amountIndex) => {
+  const RANDOM_NUMBERS_ARRAY = [];
+  while (RANDOM_NUMBERS_ARRAY.length < amountIndex) {
+    const number = getRandomPositiveInteger(1, amountIndex);
+    if(RANDOM_NUMBERS_ARRAY.indexOf(number) === -1) {RANDOM_NUMBERS_ARRAY.push(number);}
+  }
+  return RANDOM_NUMBERS_ARRAY;
+};
+
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+export {getRandomArrayOfNumbers, getRandomPositiveInteger, checkStringLength};
