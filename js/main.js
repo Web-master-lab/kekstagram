ea8eff117e8f3ccde792b7180b025b24a06ffbb7
Gleb <stepanov-gleb2003@mail.ru>
2023-03-15 11:16:12 +0300

    Подключил новые модули

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
 import './util.js';
-import {createPhotos} from './create-photo.js';
-
+import './create-photos.js';
+import './create-mini-picture.js';
+import './increase-mini-picture.js';
