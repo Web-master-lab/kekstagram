91f853af4cb553ea398d8b4dd753b71ac42598dc
Gleb <stepanov-gleb2003@mail.ru>
2023-04-10 20:17:50 +0300

    Сделал валидацию хэш-тегов

---
+++ js/form.js
@@ -0,0 +1,85 @@
+import { isSomeKey } from './util.js';
+
+const form = document.querySelector('.img-upload__form');
+const hashtagField = document.querySelector('.text__hashtags');
+const overlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const cancelButton = document.querySelector('#upload-cancel');
+const fileField = document.querySelector('#upload-file');
+const commentField = document.querySelector('.text__description');
+
+const MAX_HASHTAG_COUNT = 5;
+const UNVALID_SYMBOLS = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}$/;
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__element',
+  errorTextParent: 'img-upload__element',
+  errorTextClass: 'img-upload__error',
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeyDown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeyDown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+function onEscKeyDown (evt) {
+  if (isSomeKey('Escape')  && !isTextFieldFocused) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  showModal();
+};
+
+const hasValidSymbols = (string) => UNVALID_SYMBOLS.test(string);
+
+const isValidTag = (tag) => hasValidSymbols(tag);
+
+const hasValidCount = (tags) => tags.length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+};
+
+pristine.addValidator(
+  hashtagField,
+  validateTags,
+  'Неправильно заполнены хэштеги'
+);
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+form.addEventListener('submit', onFormSubmit);
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
