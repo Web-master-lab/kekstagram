7ffd04b71fe81deea2e5fba75db40bcf6a4c4f21
Gleb <stepanov-gleb2003@mail.ru>
2023-04-22 09:47:59 +0300

    Добавил изменения размера фото, показ/скрытие окна успеха/ошибки при загрузке фотографии на сервер. Перенес в другой модуль показ/скрытие модального окна

--- js/form.js
+++ js/form.js
@@ -1,14 +1,32 @@
 import { isSomeKey } from './util.js';
-import { currentScaleValue } from './edit-image.js';
+import { sendData } from './api.js';
+import {hideModal} from './show-form-modal.js';
 
 const form = document.querySelector('.img-upload__form');
 const hashtagField = document.querySelector('.text__hashtags');
-const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
-const cancelButton = document.querySelector('#upload-cancel');
-const fileField = document.querySelector('#upload-file');
-const commentField = document.querySelector('.text__description');
+const buttonSubmit = document.querySelector('.img-upload__submit');
+const errorWindowTemplate = document.querySelector('#error').content.querySelector('.error');
+const errorWindow = errorWindowTemplate.cloneNode(true);
+const errorCloseButton = errorWindow.querySelector('.error__button');
+const successWindowTemplate = document.querySelector('#success').content.querySelector('.success');
+const successWindow = successWindowTemplate.cloneNode(true);
+const successCloseButton = errorWindow.querySelector('.error__button');
 const scaleValue = document.querySelector('.scale__control--value');
+const buttonSmaller = document.querySelector('.scale__control--smaller');
+const buttonBigger = document.querySelector('.scale__control--bigger');
+const image = document.querySelector('.img-upload__preview').querySelector('img');
+const commentField = document.querySelector('.text__description');
+const fileField = document.querySelector('#upload-file');
+const effectSlider = document.querySelector('.effect-level__slider');
+
+let currentScaleValue = 100;
+
+errorWindow.classList.add('hidden');
+body.insertAdjacentElement('beforeend', errorWindow);
+
+successWindow.classList.add('hidden');
+body.insertAdjacentElement('beforeend', successWindow);
 
 const MAX_HASHTAG_COUNT = 5;
 const UNVALID_SYMBOLS = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}$/;
@@ -18,40 +36,6 @@ const pristine = new Pristine(form, {
   errorTextParent: 'img-upload__element',
   errorTextClass: 'img-upload__error',
 });
-
-const showModal = () => {
-  overlay.classList.remove('hidden');
-  scaleValue.value = `${currentScaleValue}%`;
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', onEscKeyDown);
-};
-
-const hideModal = () => {
-  form.reset();
-  pristine.reset();
-  overlay.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onEscKeyDown);
-};
-
-const isTextFieldFocused = () =>
-  document.activeElement === hashtagField ||
-  document.activeElement === commentField;
-
-function onEscKeyDown (evt) {
-  if (isSomeKey('Escape')  && !isTextFieldFocused) {
-    evt.preventDefault();
-    hideModal();
-  }
-}
-
-const onCancelButtonClick = () => {
-  hideModal();
-};
-
-const onFileInputChange = () => {
-  showModal();
-};
 
 const hasValidSymbols = (string) => UNVALID_SYMBOLS.test(string);
 
@@ -78,11 +62,121 @@ pristine.addValidator(
   'Неправильно заполнены хэштеги'
 );
 
+const onButtonSmallerClick = () => {
+  if (currentScaleValue !== 25) {
+    currentScaleValue-= 25;
+    image.style.transform = `scale(${currentScaleValue/100})`;
+  }
+  scaleValue.value = `${currentScaleValue}%`;
+};
+
+const onButtonBiggerClick = () => {
+  if (currentScaleValue !== 100) {
+    currentScaleValue+= 25;
+    image.style.transform = `scale(${currentScaleValue/100})`;
+  }
+  scaleValue.value = `${currentScaleValue}%`;
+};
+
+const blockSubmitButton = () => {
+  buttonSubmit.disabled = true;
+  buttonSubmit.textContent = 'Опубликовываю...';
+};
+
+const unblockSubmitButton = () => {
+  buttonSubmit.disabled = false;
+  buttonSubmit.textContent = 'Опубликовать';
+};
+
+const hideError = () => {
+  errorWindow.classList.add('hidden');
+  errorCloseButton.removeEventListener('click', hideError);
+  document.removeEventListener('keydown', onErrorEscKeyDown);
+  document.removeEventListener('click', onDocumentClickError);
+};
+
+const hideSuccess = () => {
+  successWindow.classList.add('hidden');
+  successCloseButton.removeEventListener('click', hideSuccess);
+  document.removeEventListener('keydown', onSuccessEscKeyDown);
+  document.removeEventListener('click', onDocumentClickSuccess);
+};
+
+function onErrorEscKeyDown (evt) {
+  if (isSomeKey(evt, 'Escape')) {
+    hideError();
+  }
+}
+
+function onSuccessEscKeyDown (evt) {
+  if (isSomeKey(evt, 'Escape')) {
+    hideSuccess();
+  }
+}
+
+function onDocumentClickError (evt) {
+  if (evt.target !== errorWindow) {
+    hideError();
+  }
+}
+
+function onDocumentClickSuccess (evt) {
+  if (evt.target !== errorWindow) {
+    hideSuccess();
+  }
+}
+
+const showError = () => {
+  errorWindow.classList.remove('hidden');
+
+  errorCloseButton.addEventListener('click', hideError);
+  document.addEventListener('keydown', onErrorEscKeyDown);
+  document.addEventListener('click', onDocumentClickError);
+};
+
+const showSuccess = () => {
+  successWindow.classList.remove('hidden');
+
+  successCloseButton.addEventListener('click', hideSuccess);
+  document.addEventListener('keydown', onSuccessEscKeyDown);
+  document.addEventListener('click', onDocumentClickSuccess);
+};
+
+const resetForm = () => {
+  currentScaleValue = 100;
+  image.style.transform = `scale(${currentScaleValue/100})`;
+  image.style.filter = '';
+  hashtagField.value = '';
+  commentField.value = '';
+  fileField.value = '';
+  image.className = '';
+  effectSlider.classList.add('hidden');
+};
+
 const onFormSubmit = (evt) => {
   evt.preventDefault();
-  pristine.validate();
+  const isValid = pristine.validate();
+
+  if (isValid) {
+    blockSubmitButton();
+    sendData(
+      () => {
+        hideModal();
+        showSuccess();
+        unblockSubmitButton();
+        resetForm();
+      },
+      () => {
+        showError();
+        unblockSubmitButton();
+      },
+      new FormData(evt.target)
+    );
+  }
 };
 
 form.addEventListener('submit', onFormSubmit);
-fileField.addEventListener('change', onFileInputChange);
-cancelButton.addEventListener('click', onCancelButtonClick);
+buttonBigger.addEventListener('click', onButtonBiggerClick);
+buttonSmaller.addEventListener('click', onButtonSmallerClick);
+
+export {currentScaleValue, resetForm};
