46a127f66263bfca705cbe43e6732e7debe7c185
Gleb <stepanov-gleb2003@mail.ru>
2023-04-22 09:45:57 +0300

    Поменял входные данные с случайно генерируемых на данные с сервера

--- js/create-mini-picture.js
+++ js/create-mini-picture.js
@@ -1,10 +1,9 @@
 /* eslint-disable prefer-const */
-import {createPhotos} from './create-photos.js';
 import {openBigPicture} from './open-big-picture.js';
+import { getData } from './api.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const fragment = document.createDocumentFragment();
-const photosArray = createPhotos();
 const picturesBlock = document.querySelector('.pictures');
 
 const createMiniPicture = (photo) => {
@@ -22,7 +21,7 @@ const createMiniPicture = (photo) => {
   return pictureNewTemplate;
 };
 
-const createMiniPictures = () => {
+const createMiniPictures = (photosArray) => {
   for (let i = 0; i < photosArray.length; i++) {
     const photo = createMiniPicture(photosArray[i]);
     fragment.append(photo);
@@ -30,6 +29,8 @@ const createMiniPictures = () => {
   picturesBlock.append(fragment);
 };
 
-createMiniPictures();
+getData((photos) => {
+  createMiniPictures(photos);
+});
 
 export {createMiniPictures};
