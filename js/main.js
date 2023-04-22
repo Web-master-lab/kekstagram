405c2e7843bdf803cd028a459aae18830de43c9d
Gleb <stepanov-gleb2003@mail.ru>
2023-04-22 09:48:31 +0300

    Добавил импорт для модалки и api. Убрал импорт модуля для генерации данных

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 import './util.js';
-import './create-photos.js';
 import './create-mini-picture.js';
 import './open-big-picture.js';
+import './show-form-modal.js';
 import './form.js';
 import './edit-image.js';
+import './api.js';
