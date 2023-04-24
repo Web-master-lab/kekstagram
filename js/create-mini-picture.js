93f8ae8ef60d4494c91d4bd1b57b437309672f3d
Gleb <stepanov-gleb2003@mail.ru>
2023-04-24 21:49:42 +0300

    Сделал debounce для перерисовки изображений

--- js/create-mini-picture.js
+++ js/create-mini-picture.js
@@ -1,12 +1,14 @@
 /* eslint-disable prefer-const */
 import {openBigPicture} from './open-big-picture.js';
 import { getData } from './api.js';
+import { debounce, getRandomArrayOfNumbers } from './util.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const fragment = document.createDocumentFragment();
 const picturesBlock = document.querySelector('.pictures');
+const filters = document.querySelector('.img-filters');
+const filtersForm = document.querySelector('.img-filters__form');
 
-const createMiniPicture = (photo) => {
+const createMiniPicture = (photo, fragment) => {
   const pictureNewTemplate = pictureTemplate.cloneNode(true);
   const pictureImage = pictureNewTemplate.querySelector('.picture__img');
   let pictureLikes = pictureNewTemplate.querySelector('.picture__likes');
@@ -15,6 +17,7 @@ const createMiniPicture = (photo) => {
   pictureImage.src = photo.url;
   pictureLikes.textContent = photo.likes;
   pictureComments.textContent = photo.comments.length;
+  fragment.append(pictureNewTemplate);
 
   openBigPicture(pictureNewTemplate, photo);
 
@@ -22,15 +25,64 @@ const createMiniPicture = (photo) => {
 };
 
 const createMiniPictures = (photosArray) => {
-  for (let i = 0; i < photosArray.length; i++) {
-    const photo = createMiniPicture(photosArray[i]);
-    fragment.append(photo);
-  }
+  const fragment = document.createDocumentFragment();
+
+  photosArray.forEach((photo) => {
+    createMiniPicture(photo, fragment);
+  });
+
   picturesBlock.append(fragment);
+};
+
+const compareComments = (photoA, photoB) => {
+  const commentsA = photoA.comments.length;
+  const commentsB = photoB.comments.length;
+
+  return commentsB - commentsA;
+};
+
+const filterPhotos = (evt, photos) => {
+  const miniPictures = document.querySelectorAll('.picture');
+  miniPictures.forEach((miniPicture) => miniPicture.remove());
+
+  if (evt.target.id === 'filter-default') {
+    createMiniPictures(photos);
+  }
+
+  if (evt.target.id === 'filter-random') {
+    const fragment = document.createDocumentFragment();
+
+    getRandomArrayOfNumbers(photos.length - 1).slice(0, 10)
+      .forEach((number) => {
+        createMiniPicture(photos[number], fragment);
+      });
+
+    picturesBlock.append(fragment);
+  }
+
+  if (evt.target.id === 'filter-discussed') {
+    const sortPhotos = photos.slice().sort(compareComments);
+    createMiniPictures(sortPhotos);
+  }
+};
+
+const showFilters = (photos, cb) => {
+  filters.classList.remove('img-filters--inactive');
+  filtersForm.addEventListener('click', (evt) => {
+    filtersForm.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+
+    evt.target.classList.add('img-filters__button--active');
+    cb(evt, photos);
+  });
 };
 
 getData((photos) => {
   createMiniPictures(photos);
+  showFilters(photos,
+    debounce(
+      (evt) => filterPhotos(evt, photos),
+      500
+    ));
 });
 
 export {createMiniPictures};
